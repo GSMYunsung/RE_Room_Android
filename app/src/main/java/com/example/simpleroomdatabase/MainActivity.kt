f394bf0a24fe10cf9236c02bb1fcea6e3ac0b277
yunsung <12345ggh81@gmail.com>
2021-06-29 21:13:20 +0900

    [CRATE] 구성요소 업데이트

---
+++ app/src/main/java/com/example/simpleroomdatabase/MainActivity.kt
@@ -0,0 +1,34 @@
+    package com.example.simpleroomdatabase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simpleroomdatabase.Room.DataBase
+import com.example.simpleroomdatabase.databinding.ActivityMainBinding
+
+    class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val todoDataBase = DataBase.getInstanceDataBase(applicationContext)
+        val databaseall = todoDataBase?.todoDao()?.getall()
+
+        binding =  DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val viewManager = LinearLayoutManager(this)
+        val todoAdapter = TodoMainAdapter(ArrayList(databaseall),todoDataBase)
+
+        binding.mainRecyclerview.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = todoAdapter
+        }
+
+        binding.mainButton.setOnClickListener {
+            startActivity(Intent(this,TodoActivity::class.java))
+        }
+    }
+}
